df37287e3617f12192b66b6803f3ac057cb6761a
4LG0R <crlsnyaga@gmail.com>
2018-10-27 15:40:40 +0300

    Adding user signup form....warathose.....

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 50 }
+    #validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+    validates :password, presence: true, length: { maximum: 50, minimum: 6 }
+    validates :password_confirmation, presence: true, length: { minimum: 6 }
+    #validates :password, confirmation: true, length { maximum: 50 ,minimum: 6}
+    validates_presence_of :name, :password, :password_confirmation
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_122145) do
+ActiveRecord::Schema.define(version: 2018_10_13_140716) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_09_29_122145) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_confirmation"
   end
 
 end
